ca4248de575d361cb169e244466bcd28c80d454d
Alexandre SCHMITT-BURTSCHELL <venatcher@gmail.com>
2014-08-15 21:33:44 +0200

    Suppression de la prise en charge 'lo' et NETS
    Variable "nets" placée globalement et suppression de la prise en charge
    du périphérique 'lo' sur Linux.

--- ruby-net-scanner/net-scanner.rb
+++ ruby-net-scanner/net-scanner.rb
@@ -2,12 +2,12 @@ require_relative 'mod-OS'
 
 def getInterfaces
 	#Sur Windows, utilisation de WMI
+	nets = Array.new
 	if OS.windows?
 		require 'win32ole'
 		wmi = WIN32OLE.connect("winmgmts://")
 
 		interfaces = wmi.ExecQuery("SELECT * FROM Win32_NetworkAdapterConfiguration Where IPEnabled = True")
-		nets = Array.new
 		for interface in interfaces do
 			net = Hash.new
 			net['ip'] = interface.IPAddress
@@ -18,7 +18,7 @@ def getInterfaces
 		    nets.push(net)
 		end
 	elsif OS.linux?
-		interfaces = `ip -o addr show | awk '/inet/ {print $4}'`.split("\n")
+		interfaces = `ip -o addr show | grep -v ": lo" | awk '/inet/ {print $4}'`.split("\n")
 		for interface in interfaces do
 			net = Hash.new
 			# Cast de l'IP et du masque en Array pour respecter le format Microsoft
@@ -36,6 +36,7 @@ def scanNet
 	interfaces.each{|i|
 		interface = IPAddr.new i['ip'].first
 		interface = interface.mask(i['mask'].first)
+
 		range = interface.to_range().to_a
 		range.shift #delete first IP (0.0) (No valid)
 		range.pop #Delete last IP broadcast (No valid)
